c618b6c0dc886345e04f6d81a4e37f3973c090cd
Tigloo <collajm@hotmail.com>
2022-07-25 15:01:22 +0200

    modification csrf

--- src/Tigloo/View/TwigExtension.php
+++ src/Tigloo/View/TwigExtension.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 namespace Tigloo\View;
 
 use Tigloo\Container\Contracts\ContainerInterface;
+use Tigloo\Core\Session;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
@@ -20,11 +21,18 @@ class TwigExtension extends AbstractExtension
     {
         return [
             new TwigFunction('Route', [$this, 'generateUrlOfRoute']),
+            new TwigFunction('Csrf', [$this, 'csrfGenerator'])
         ];
     }
 
     public function generateUrlOfRoute(string $name, array $attributes = [])
     {
         return $this->app->get('router')->generate($name, $attributes);
+    }
+
+    public function csrfGenerator(): string
+    {
+        $session = new Session();
+        return '<input type="hidden" name="csrf" value="'.$session->get('csrf_token').'">';
     }
 }
